263ac8efcb23eac6c7228aecb66c17f56a2e2d45
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2022-07-13 20:50:14 -0300

    primer commit

--- script.js
+++ script.js
@@ -19,43 +19,77 @@ const productos = [producto1, producto2, producto3, producto4]
 
 console.log(productos)
 
-let preguntar = prompt("¿Desea agregar mas producto?")
-    if(preguntar == "si"){
+let preguntar,preguntarCuantasVeces,preguntar2,productoEliminar,indice
 
-        let preguntarCuantasVeces = parseInt(prompt("Especifique en numeros cuantas prendas va a agregar"))
-
-        for(i = 1; i <= preguntarCuantasVeces; i++){
-            productos.push(new Producto)
-              
-            console.log(productos)
-        }
+    preguntar = prompt("¿Desea agregar mas producto?")
+    
+    while(preguntar == ""){
         
-    }else if(preguntar == "no"){
-        let preguntar2 = prompt("¿Desea eliminar un producto?")
-
-            if(preguntar2 == "no"){
-                alert("Gracias por usar mi herramienta")
-                console.log(productos)
-
-            }else if(preguntar2 == "si"){
-                let productoEliminar = prompt("Ingrese el indice del producto que quiere eliminar")
-                let indice = productoEliminar
-
-                if((indice != -1) && (indice < productos.length)) {
-                    productos.splice(indice,1)
-                    console.log(productos)
-
-                }else{
-                    alert("Producto no encontrado")
-                    console.log(productos)
-                }
-            }
+        preguntar = prompt("¿Desea agregar mas producto?")
     }
     
+    
+        if(preguntar == "si"){
 
+            console.log(productos);
 
+            preguntarCuantasVeces = parseInt(prompt("Especifique en numeros cuantas prendas va a agregar"))
 
+            for(i = 1; i <= preguntarCuantasVeces; i++){
+                productos.push(new Producto)
+                
+                console.log(productos)
+            }
+            
+        }else if(preguntar == "no"){
+            
 
+             preguntar2 = prompt("¿Desea eliminar un producto?")
 
+             while(preguntar2 == ""){
 
+                preguntar2 = prompt("¿Desea eliminar un producto?")
 
+             }
+
+            
+                if(preguntar2 == "no"){
+                    alert("Gracias por usar mi herramienta")
+                    console.log(productos)
+
+                }else if(preguntar2 == "si"){
+                     productoEliminar = prompt("Ingrese el indice del producto que quiere eliminar")
+
+                        while(productoEliminar == ""){
+
+                            productoEliminar = prompt("Ingrese el indice del producto que quiere eliminar")
+
+                        }
+
+                     indice = productoEliminar
+                    
+                     
+                    if((indice != -1) && (indice < productos.length)) {
+                        productos.splice(indice,1)
+                        console.log(productos)
+                        alert("Listo!. Gracias por usar mi herramienta")
+
+                        
+                        
+                    }else if((indice >= -1) && (indice > productos.length)){
+                        alert("Producto no encontrado")
+                        console.log(productos) 
+
+                        productoEliminar = prompt("Ingrese un indice valido")
+                        indice = productoEliminar
+
+                        if((indice != -1) && (indice < productos.length)) {
+                            productos.splice(indice,1)
+                            console.log(productos)
+                            alert("Listo!. Gracias por usar mi herramienta")
+
+                    }
+                }
+            }
+        }
+        
